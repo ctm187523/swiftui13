8d7e04706f2b37c76e63c12e7f91b23582dd5b01
ctm187523 <58368810+ctm187523@users.noreply.github.com>
2020-12-17 19:08:48 +0100

    Proyecto de prueba

---
+++ prueba/prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Clemente Tiñena Marieges on 17/12/20.
+//  Copyright © 2020 Clemente Tiñena Marieges. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
